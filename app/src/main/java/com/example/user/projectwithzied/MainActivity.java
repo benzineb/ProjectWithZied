81a16567dd34171872d7bcf02ad100cae7b90ddf
benzineb <benzinebjabeur@gmail.com>
2016-04-18 01:45:03 +0100

    Connexion et lecture réussite à la base données

--- app/src/main/java/com/example/user/projectwithzied/MainActivity.java
+++ app/src/main/java/com/example/user/projectwithzied/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.user.projectwithzied;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,17 +15,19 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class MainActivity extends AppCompatActivity {
     private String[] arraySpinner;
     static final int dp=0;
     static final int arr=0;
-//Override
- /*   public void onSaveInstanceState(Bundle outState) {
-      //  outState.putInt(dp);
+    Cursor c=null;
+public static String GareDepart;
+    Integer IndexGareArrivee,IndexGareDepart;
+    String headsign ;
 
 
-        super.onSaveInstanceState(outState);
-    }*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,24 +36,72 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Button partir= (Button) findViewById(R.id.maintenant);
-        Button partirpl= (Button) findViewById(R.id.maintenant);
+        Button partirpl= (Button) findViewById(R.id.partirpl);
         this.arraySpinner = new String[] {
-                "Mahdia La Gare ", "EZZAHRA", "Borj Arif", "Sidi Messaoud", "Zone Touristique"
+                "Mahdia ", "Ezzahra", "Borj Arif", "Sidi Massaoud", "Mahdia Z.T."
         };
        final  Spinner dep = (Spinner) findViewById(R.id.spinner);
         final Spinner arr = (Spinner) findViewById(R.id.spinnerarr);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_spinner_item, arraySpinner);
         dep.setAdapter(adapter);
         arr.setAdapter(adapter);
-        partir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (dep.getSelectedItem()==arr.getSelectedItem()){
-                    Toast.makeText(MainActivity.this,"la station du départ doit être differente de l'arrivée ",Toast.LENGTH_LONG).show();
+        GareDepart=dep.getSelectedItem().toString();
+        IndexGareDepart=dep.getSelectedItemPosition();
+        IndexGareArrivee=arr.getSelectedItemPosition();
+        if (IndexGareDepart<IndexGareArrivee){
+            headsign="Sousse";
+        }else {
+            headsign="Mahdia";
+        }
+
+    partir.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (dep.getSelectedItem() == arr.getSelectedItem()) {
+                Toast.makeText(MainActivity.this, "la station du départ doit être differente de l'arrivée ", Toast.LENGTH_LONG).show();
+            }
+            else{
+                DataBaseHelper myDbHelper = new DataBaseHelper(MainActivity.this);
+
+
+                try {
+
+                    myDbHelper.createDataBase();
+
+                } catch (IOException ioe) {
+
+                    throw new Error("Unable to create database");
+
+                }
+
+                try {
+
+                    myDbHelper.openDataBase();
+
+                } catch (SQLException sqle) {
+
+                    try {
+                        throw sqle;
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+               c = myDbHelper.query("stop_times,stops",new String[]{"departure_time"}, "stop_times.stop_id=stops.stop_id AND stops.stop_name="+GareDepart+" and trip_id<43 AND stop_times.stop_headsign="+headsign+"", null, null, null,"departure_time");
+//c=myDbHelper.query("stop_times,stops",null,null,null,null,null,null);
+                if (c.moveToFirst()) {
+                    do {
+                        Toast.makeText(MainActivity.this,
+                                "departure_time=" + c.getString(0), Toast.LENGTH_LONG).show();
+                    } while (c.moveToNext());
+
                 }
             }
-        });
+        }
+    });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
     }
 
 
@@ -82,5 +134,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public static String getGareDepart() {
+        return GareDepart;
+    }
+
+    public String getHeadsign() {
+        return headsign;
     }
 }
